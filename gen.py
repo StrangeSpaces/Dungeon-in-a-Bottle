f20677f99d0280cbfb81a58fcd94374bdfa9707d
Andew Jones <andrew.joe.jones@gmail.com>
2017-04-23 22:29:36 -0400

    levels

--- gen.py
+++ gen.py
@@ -5,6 +5,7 @@ os.chdir('levels')
 final = 'levels = [\n'
 
 files = os.listdir(os.getcwd())
+files = [f for f in files if f[0] != '.']
 
 for i in xrange(len(files)):
     with open('CompactLevel{}.json'.format(i+1)) as f:
